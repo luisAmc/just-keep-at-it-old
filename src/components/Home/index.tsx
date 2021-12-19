e4237cfc345d0fc89542d967ccbe52438c2469d1
Luis Martinez <luis.kosm@gmail.com>
2021-12-19 15:56:48 -0600

    Adds tailwindcss

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,9 @@
+export function Home() {
+  return (
+    <div className='h-screen max-w-2xl w-full mx-auto flex'>
+      <div className='flex-1 grid place-items-center'>
+        <button className="px-4 py-3 bg-blue-500 text-white rounded-md shadow-lg shadow-blue-500/50">Just a centered button!</button>
+      </div>
+    </div>
+  );
+}
