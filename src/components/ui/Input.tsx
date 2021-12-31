af33ea794e6c32c79bea0472369a2ad005bb8f48
Luis Martinez <luis.kosm@gmail.com>
2021-12-31 14:28:17 -0600

    Refactor Input component to a function instead of an anonymous arrow function to prevent eslint's displayName error

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -6,25 +6,26 @@ interface Props extends ComponentProps<'input'> {
   label: string;
 }
 
-export const Input = forwardRef<HTMLInputElement, Props>(
-  ({ label, hideLabel = false, type = 'text', ...props }, ref) => {
-    return (
-      <label>
-        {!hideLabel && (
-          <div className='font-medium text-gray-800 mb-1'>{label}</div>
-        )}
-        <input
-          className='bg-white text-gray-800 w-full rounded-md px-4 py-2 border border-gray-200 focus:outline-none focus:border-brand-600 focus:border-2 focus:ring-brand-500 disabled:opacity-60 disabled:bg-gray-500 disabled:bg-opacity-20'
-          type={type}
-          step={type === 'number' ? 'any' : undefined}
-          ref={ref}
-          autoComplete={props.autoComplete || 'off'}
-          placeholder={`${label}...`}
-          {...props}
-        />
+export const Input = forwardRef<HTMLInputElement, Props>(function Input(
+  { label, hideLabel = false, type = 'text', ...props },
+  ref
+) {
+  return (
+    <label>
+      {!hideLabel && (
+        <div className='font-medium text-gray-800 mb-1'>{label}</div>
+      )}
+      <input
+        className='bg-white text-gray-800 w-full rounded-md px-4 py-2 border border-gray-200 focus:outline-none focus:border-brand-600 focus:border-2 focus:ring-brand-500 disabled:opacity-60 disabled:bg-gray-500 disabled:bg-opacity-20'
+        type={type}
+        step={type === 'number' ? 'any' : undefined}
+        ref={ref}
+        autoComplete={props.autoComplete || 'off'}
+        placeholder={`${label}...`}
+        {...props}
+      />
 
-        <FieldError name={props.name} />
-      </label>
-    );
-  }
-);
+      <FieldError name={props.name} />
+    </label>
+  );
+});
