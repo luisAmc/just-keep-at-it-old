b39387d88895c05e8c25b02e2859600994b8c78f
Luis Martinez <luis.kosm@gmail.com>
2022-09-12 21:50:36 -0600

    Removes continue's page

--- src/pages/workouts/[workoutId]/get-it-done/continue.tsx
+++
@@ -1,8 +0,0 @@
-import { GetServerSideProps } from 'next';
-import { GetItDone, query } from 'src/components/Workouts/GetItDone';
-import { preloadQuery } from 'src/utils/apollo';
-
-export const getServerSideProps: GetServerSideProps = async (ctx) =>
-  preloadQuery(ctx, { query });
-
-export default GetItDone;
