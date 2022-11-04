e58c4964a120a6f4e8b61d4ccb9ef60f2cbb66d9
Luis Martinez <luis.kosm@gmail.com>
2022-11-04 17:20:05 -0600

    Adds delete from get-it-done

--- src/components/Workouts/GetItDone/index.tsx
+++ src/components/Workouts/GetItDone/index.tsx
@@ -5,7 +5,11 @@ import { Form, useZodForm } from 'src/components/shared/Form';
 import { array, z, literal, object, string } from 'zod';
 import { useFieldArray, useWatch } from 'react-hook-form';
 import { Button } from 'src/components/shared/Button';
-import { CheckCircleIcon, ChevronLeftIcon } from '@heroicons/react/outline';
+import {
+  CheckCircleIcon,
+  ChevronLeftIcon,
+  TrashIcon
+} from '@heroicons/react/outline';
 import { Heading } from 'src/components/shared/Heading';
 import { WorkoutExercise } from './WorkoutExercise';
 import { useModal } from 'src/components/shared/Modal';
@@ -16,10 +20,13 @@ import { LastExerciseSessions } from './LastExerciseSessions';
 import { useSlideOver } from 'src/components/shared/SlideOver';
 import { useEffect, useState } from 'react';
 import {
+  GetItDoneDeleteMutation,
+  GetItDoneDeleteMutationVariables,
   GetItDoneQuery,
   GetWorkoutDoneMutation,
   GetWorkoutDoneMutationVariables
 } from './__generated__/index.generated';
+import { ConfirmationModal } from 'src/components/shared/ConfirmationModal';
 
 const WorkoutExerciseFragment = gql`
   fragment WorkoutExercise_workoutExercise on WorkoutExercise {
@@ -85,6 +92,7 @@ const GetItDoneSchema = object({
 export function GetItDone() {
   const router = useRouter();
   const workoutId = router.query.workoutId as string;
+  const deleteModal = useModal();
 
   const addExerciseModal = useModal();
   const lastSessions = useSlideOver();
@@ -140,6 +148,25 @@ export function GetItDone() {
       onCompleted() {
         localStorage.removeItem(`workout-${workoutId}`);
         router.push(`/workouts/${workoutId}`);
+      }
+    }
+  );
+
+  const [deleteCommit] = useMutation<
+    GetItDoneDeleteMutation,
+    GetItDoneDeleteMutationVariables
+  >(
+    gql`
+      mutation GetItDoneDeleteMutation($workoutId: ID!) {
+        deleteWorkout(workoutId: $workoutId) {
+          id
+        }
+      }
+    `,
+    {
+      onCompleted() {
+        localStorage.removeItem(`workout-${workoutId}`);
+        router.replace('/');
       }
     }
   );
@@ -204,14 +231,38 @@ export function GetItDone() {
 
       {workout && (
         <div className='h-full flex flex-col space-y-4'>
-          <div className='flex items-center space-x-2'>
-            <Button href='/' className=''>
-              <div className='rounded-full bg-brand-300 text-brand-700 p-2 flex items-center justify-center'>
-                <ChevronLeftIcon className='w-4 h-4' />
-              </div>
-            </Button>
+          <div className='flex items-center justify-between'>
+            <div className='flex items-center space-x-2'>
+              <Button href='/' className=''>
+                <div className='rounded-full bg-brand-300 text-brand-700 p-2 flex items-center justify-center'>
+                  <ChevronLeftIcon className='w-4 h-4' />
+                </div>
+              </Button>
 
-            <Heading>{workout.name}</Heading>
+              <Heading>{workout.name}</Heading>
+            </div>
+
+            <div>
+              <Button
+                onClick={deleteModal.open}
+                className='p-2 rounded-full bg-rose-500 text-rose-50 border border-rose-200 focus:outline-none focus:ring-2 focus:ring-offset-white focus:ring-offset-1 hover:bg-opacity-80'
+              >
+                <TrashIcon className='w-4 h-4' />
+              </Button>
+
+              <ConfirmationModal
+                {...deleteModal.props}
+                onConfirm={() => {
+                  deleteCommit({
+                    variables: {
+                      workoutId: workoutId
+                    }
+                  });
+                }}
+              >
+                ¿Está seguro de borrar la rutina?
+              </ConfirmationModal>
+            </div>
           </div>
 
           <Form form={form} onSubmit={onSubmit}>
