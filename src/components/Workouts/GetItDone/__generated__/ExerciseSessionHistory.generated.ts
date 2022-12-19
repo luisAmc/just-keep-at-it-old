861487a7b2f6fe35305a18656e2c3c3d68c0b96f
Luis Martinez <luis.kosm@gmail.com>
2022-12-18 21:04:39 -0600

    Remove old GetItDone

--- src/components/Workouts/GetItDone/__generated__/ExerciseSessionHistory.generated.ts
+++
@@ -1,9 +0,0 @@
-import * as Types from '../../../../__generated__/schema.generated';
-
-export type ExerciseSessionHistoryQueryVariables = Types.Exact<{
-  id: Types.Scalars['ID'];
-  limit?: Types.InputMaybe<Types.Scalars['Int']>;
-}>;
-
-
-export type ExerciseSessionHistoryQuery = { __typename?: 'Query', exercise: { __typename?: 'Exercise', id: string, name: string, type: string, doneSessions: Array<{ __typename?: 'WorkoutExercise', id: string, sets: Array<{ __typename?: 'WorkoutSet', id: string, mins: number, distance: number, kcal: number, lbs: number, reps: number }>, workout: { __typename?: 'Workout', name: string, completedAt?: string | null } }> } };
--- src/components/Workouts/GetItDone/__generated__/Workout.generated.ts
+++
@@ -1,8 +0,0 @@
-import * as Types from '../../../../__generated__/schema.generated';
-
-export type GetItDoneWorkoutMutationVariables = Types.Exact<{
-  input: Types.GetWorkoutDoneInput;
-}>;
-
-
-export type GetItDoneWorkoutMutation = { __typename?: 'Mutation', getWorkoutDone: { __typename?: 'Workout', id: string } };
