27cf25b43a49ca803155e7068c80cd97b905c96c
luisAmc <luis.kosm@gmail.com>
2024-03-14 19:47:31 -0600

    Updates service worker behaviour of cache

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
+/** @type {import('next').NextConfig} */
 const runtimeCaching = require('next-pwa/cache');
+
+const nextDataIndex = runtimeCaching.findIndex(
+  (entry) => entry.options.cacheName === 'next-data'
+);
+
+if (nextDataIndex !== -1) {
+  runtimeCaching[nextDataIndex].handler = 'NetworkFirst';
+} else {
+  throw new Error('Failed to find next-data object in runtime caching');
+}
 
 const withPWA = require('next-pwa')({
   dest: 'public',
+  runtimeCaching,
   register: true,
   skipWaiting: true,
-  runtimeCaching,
-  disable: process.env.NODE_ENV !== 'production'
+  // disable: process.env.NODE_ENV === 'development'
 });
 
 const nextConfig = {
